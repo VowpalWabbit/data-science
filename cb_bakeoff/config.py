cb2411bef841900aad492e92fc14ec6999a9a4c8
pavithraks <pavithraks@gmail.com>
2020-04-17 13:17:16 -0400

    modified config file

--- cb_bakeoff/config.py
+++ cb_bakeoff/config.py
@@ -6,7 +6,7 @@ import time
 
 np.random.seed(int(time.time()))
 
-'''
+
 # batch account info
 _BATCH_ACCOUNT_NAME ='' # Your batch account name 
 _BATCH_ACCOUNT_KEY = '' # Your batch account key
@@ -15,14 +15,7 @@ _BATCH_ACCOUNT_URL = '' # Your batch account URL
 # storage account info
 _STORAGE_ACCOUNT_NAME = '' # Your storage account name
 _STORAGE_ACCOUNT_KEY = ''# Your storage account keys
-'''
 
-_BATCH_ACCOUNT_NAME ='dsdatasets' # Your batch account name 
-_BATCH_ACCOUNT_KEY = 'U6wdi73GfgJald0KWZjy/A+WAb5rkcuzIcrhY6yLp8bGS/Irt0X1p3ZKgURm5B/rsvazXFz1GnuBHB35X1UsIQ==' # Your batch account key
-_BATCH_ACCOUNT_URL = 'https://dsdatasets.eastus.batch.azure.com' # Your batch account URL
-
-_STORAGE_ACCOUNT_NAME = 'dsdatasetseus01'
-_STORAGE_ACCOUNT_KEY = 'WyWE/VqEwm4WhSy4b887tNn6XvYBFToFKGPxSD7MzfjEodO8ultKcFMgHzqTjBckRNfugRGJEPwzQOwD+yR7+Q=='#
 
 # other script specific configs
 # you can skip the pool creation step in line 407 of run_azure_batch_job.py and instead set up a custom pool
