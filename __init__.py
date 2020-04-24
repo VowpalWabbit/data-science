4d4c653579b138bcd768382f73e4712e40cb5fde
dasommer <dasommer@microsoft.com>
2020-04-24 20:44:44 +0000

    Minor fixes in README.md and removes extraneous copies of vw_transforms code

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-from .tabular_to_dsjson import patch_dsjson
-from .tabular_to_dsjson import convert_to_dsjson
-from .vw_tabular import extract_csv
-from .vw_tabular import extract_pandas
-from .converter_common import default_convert_config, default_patch_config, patch_only_config
