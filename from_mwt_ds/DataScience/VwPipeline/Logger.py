9845c74f9abbf7171097e5aa9f814b0d50b6fafb
Alexey Taymanov <ataymano@gmail.com>
2020-12-11 00:49:24 -0500

    logger cleanup

--- from_mwt_ds/DataScience/VwPipeline/Logger.py
+++ from_mwt_ds/DataScience/VwPipeline/Logger.py
@@ -4,28 +4,33 @@ import time
 from threading import Lock
 
 
-class console_logger:
+class ConsoleLogger:
     def __init__(self, level: str = 'INFO'):
         self.level = logging.getLevelName(level)
         self.lock = Lock()
 
-    def debug(self, message: str):
-        if self.level <= logging.DEBUG: self._trace(message)
+    def debug(self, message: str, prefix: str = None):
+        if self.level <= logging.DEBUG:
+            self.__trace__(message, prefix)
 
-    def info(self, message: str):
-        if self.level <= logging.INFO: self._trace(message)
+    def info(self, message: str, prefix: str = None):
+        if self.level <= logging.INFO:
+            self.__trace__(message, prefix)
 
-    def warning(self, message: str):
-        if self.level <= logging.WARNING: self._trace(message)
+    def warning(self, message: str, prefix: str = None):
+        if self.level <= logging.WARNING:
+            self.__trace__(message, prefix)
 
-    def error(self, message: str):
-        if self.level <= logging.ERROR: self._trace(message)
+    def error(self, message: str, prefix: str = None):
+        if self.level <= logging.ERROR:
+            self.__trace__(message, prefix)
 
-    def critical(self, message: str):
-        if self.level <= logging.CRITICAL: self._trace(message)
+    def critical(self, message: str, prefix: str = None):
+        if self.level <= logging.CRITICAL:
+            self.__trace__(message, prefix)
 
-    def _trace(self, message: str):
-        prefix = f'[{time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))}] '
+    def __trace__(self, message: str, prefix: str):
+        prefix = f'{prefix}[{time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))}] '
         self.lock.acquire()
         print(prefix + message)
         self.lock.release()
