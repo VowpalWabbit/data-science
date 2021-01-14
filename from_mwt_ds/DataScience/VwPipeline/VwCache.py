3704c44077f6579efae6e4348d085b3510fd57fe
Alexey Taymanov <ataymano@microsoft.com>
2021-01-14 11:06:11 -0500

    Logger -> Loggers

--- from_mwt_ds/DataScience/VwPipeline/VwCache.py
+++ from_mwt_ds/DataScience/VwPipeline/VwCache.py
@@ -18,7 +18,7 @@ class VwCache:
         os.makedirs(folder_name, exist_ok=True)
         return os.path.join(context, VwCache.__file_name__(args_hash))
 
-    def get_rel_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger = Logger.ConsoleLogger()) -> str:
+    def get_rel_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger = Loggers.ConsoleLogger()) -> str:
         from VwPipeline import VwOpts
         opts = VwOpts.to_string(opts_in)
         if salt:
@@ -28,5 +28,5 @@ class VwCache:
         logger.debug(f'Generating path for opts_in: {VwOpts.to_string(opts_in)}, opt_out: {opt_out}. Result: {result}')
         return result
 
-    def get_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger = Logger.ConsoleLogger()) -> str:
+    def get_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger = Loggers.ConsoleLogger()) -> str:
         return os.path.join(self.Path, self.get_rel_path(opts_in, opt_out, salt, logger))
