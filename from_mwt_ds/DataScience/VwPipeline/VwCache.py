534c85f4d1b798f649e9558a7a90a8d1c4e74724
Alexey Taymanov <ataymano@gmail.com>
2021-01-20 12:01:14 -0500

    forgotten changes

--- from_mwt_ds/DataScience/VwPipeline/VwCache.py
+++ from_mwt_ds/DataScience/VwPipeline/VwCache.py
@@ -5,8 +5,8 @@ from VwPipeline import Loggers
 
 class VwCache:
     def __init__(self, path: str):
-        self.Path = path
-        os.makedirs(self.Path, exist_ok=True)
+        self.path = path
+        os.makedirs(self.path, exist_ok=True)
 
     @staticmethod
     def _file_name(string_hash: str) -> str:
@@ -14,19 +14,19 @@ class VwCache:
         return hashlib.md5(string_hash.encode('utf-8')).hexdigest()
 
     def _get_path(self, context: str, args_hash: str) -> str:
-        folder_name = os.path.join(self.Path, context)
+        folder_name = os.path.join(self.path, context)
         os.makedirs(folder_name, exist_ok=True)
         return os.path.join(context, VwCache._file_name(args_hash))
 
-    def get_rel_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger=Loggers.ConsoleLogger()) -> str:
+    def get_rel_path(self, opts: dict, output: str = None, salt: str = None, logger=Loggers.ConsoleLogger()) -> str:
         from VwPipeline import VwOpts
-        opts = VwOpts.to_string(opts_in)
+        opts_str = VwOpts.to_string(opts)
         if salt:
-            opts = opts + f' -# {salt}'
-        args_hash = VwOpts.string_hash(opts)
-        result = self._get_path(f'cache{opt_out}', args_hash)
-        logger.debug(f'Generating path for opts_in: {VwOpts.to_string(opts_in)}, opt_out: {opt_out}. Result: {result}')
+            opts_str = opts_str + f' -# {salt}'
+        args_hash = VwOpts.string_hash(opts_str)
+        result = self._get_path(f'cache{output}', args_hash)
+        logger.debug(f'Generating path for opts: {VwOpts.to_string(opts)}, output: {output}. Result: {result}')
         return result
 
-    def get_path(self, opts_in: dict, opt_out: str = None, salt: str = None, logger=Loggers.ConsoleLogger()) -> str:
-        return os.path.join(self.Path, self.get_rel_path(opts_in, opt_out, salt, logger))
+    def get_path(self, opts: dict, output: str = None, salt: str = None, logger=Loggers.ConsoleLogger()) -> str:
+        return os.path.join(self.path, self.get_rel_path(opts, output, salt, logger))
