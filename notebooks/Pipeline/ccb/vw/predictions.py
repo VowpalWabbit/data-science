d2f5cef8413ab21eec287ccd6368598f92deb940
Alexey Taymanov <ataymano@gmail.com>
2021-04-23 08:58:43 -0400

    vw predictions fileset

--- notebooks/Pipeline/ccb/vw/predictions.py
+++ notebooks/Pipeline/ccb/vw/predictions.py
@@ -1,6 +1,8 @@
 import json
 import pandas as pd
 
+from Pipeline.dataflow import Fileset
+
 def line_2_slot(line):
     return {int(p.split(':')[0]) : float(p.split(':')[1])  for p in line.split(',')}
 
@@ -30,3 +32,20 @@ def lines_2_predictions_df(vw_predictions, pipeline_predictions, policy_name):
     pipeline_predictions['_tmp'] = list(lines_2_slots(vw_predictions))
     pipeline_predictions[('b', policy_name)] = pipeline_predictions.apply(lambda r: [ap[1][ap[0]] for ap in zip(r['a'], r['_tmp'])], axis = 1)
     return pipeline_predictions[['t', 'a', 'r', 'p', 'n', ('b', policy_name)]]
+
+class VwPredicionsFiles(Fileset):
+    def _read(self, i, path):
+        print(f'{i}: {path}')
+        labels = self.label_fileset.read(i)
+        labels['_tmp'] = list(lines_2_slots(open(path)))
+        labels[('b', self.policy_name)] = labels.apply(lambda r: [ap[1][ap[0]] for ap in zip(r['a'], r['_tmp'])], axis = 1)
+        return labels[['t', 'a', 'r', 'p', 'n', ('b', self.policy_name)]]        
+
+    @staticmethod
+    def _write(path, o):
+        raise Exception('Not supported')
+
+    def __init__(self, files, label_fileset, policy_name):
+        super().__init__(files=files, reader=self._read, writer=VwPredicionsFiles._write)
+        self.label_fileset = label_fileset
+        self.policy_name = policy_name
