7b8f9a1baab054300b2ee1c99b314688f93b949f
Alexey Taymanov <ataymano@gmail.com>
2021-04-23 10:46:40 -0400

    estimators fixes

--- notebooks/Pipeline/cb/estimators.py
+++ notebooks/Pipeline/cb/estimators.py
@@ -1,21 +1,27 @@
 import json
+import scipy.stats
+import math
 
 class ips_snips:
-    def __init__(self, r = 0, n = 0, n_ips = 0):
+    def __init__(self, r = 0, n = 0, n_ips = 0, r2 = 0):
         self.r = r
         self.n = n
         self.n_ips = n_ips
+        self.r2 = r2
 
     def add(self, r, p_log, p, n = 1):
-        self.r += n * r * p / p_log
-        self.n += n
+        num = r * p / p_log
+        self.r += n * num
+        self.n += n * int(p > 0)
         self.n_ips += n * p / p_log
+        self.r2 += n * num**2
 
     def __add__(self, other):
         r = self.r + other.r
         n = self.n + other.n
+        r2 = self.r2 + other.r2
         n_ips = self.n_ips + other.n_ips
-        return ips_snips(r, n, n_ips)
+        return ips_snips(r, n, n_ips, r2)
 
     def __str__(self):
         return f'r: {self.r}, n: {self.n}, n_ips: {self.n_ips}'
@@ -23,11 +29,22 @@ class ips_snips:
     def name(self):
         return 'ips_snips'
 
-    def get(self, type):
+    def get_ips(self):
+        return 0 if self.n == 0 else self.r / self.n
+
+    def get_snips(self):
+        return 0 if self.n == 0 else self.r / self.n_ips
+
+    def get(self, type, alpha=0.05):
         if type == 'ips':
-            return {'e': 0 if self.n == 0 else self.r / self.n}
+            return {'e': self.get_ips()}
         elif type == 'snips':
-            return {'e': 0 if self.n == 0 else self.r / self.n_ips}
+            return {'e': self.get_snips()}
+        elif type == 'gaussian':
+            ppf = scipy.stats.norm.ppf(1 - alpha/2)
+            variance = (self.r2 - self.r**2 / self.n) / (self.n - 1)
+            delta = ppf * math.sqrt(variance/self.n)
+            return {'l': self.get_ips() - delta, 'u': self.get_ips() + delta}
 
 class ips:
     def __init__(self, r = 0, n = 0):
