2e33050750e2c1264c42af5d23ff9ad0e66b1426
kammitama5 <krystal.maughan@gmail.com>
2021-07-03 11:21:42 -0400

    Remove comments

--- from_mwt_ds/DataScience/estimate.py
+++ from_mwt_ds/DataScience/estimate.py
@@ -1,49 +1,9 @@
 import pandas as pd
-
-#  Way to workaround while it is not in pip
-#  1. clone estimators/rlos2021_cfe, python3 setup.py install
-#  2. copy estimators folder from rlos2021_cfe branch of estimators repo
-# import estimators 
+import estimators
+from bandits import ips
 
 def estimate_bucket(bucket: pd.DataFrame, config: dict) -> dict: 
     if len(bucket) == 0 or config == {}:
         return {}
     else:
-        return_dict = {}
-        for policies in config:
-            firstpart = list(config.get(policies).keys()[0])
-            for i in config.get(policies):
-                secondpart = list(config.get(policies).values())[0][0]
-        keyresult = firstpart + "_" + secondpart
-        formula_ips = (df["r"][0] * df["policy_1"][0] / df["p"][0]) / 1
-        return_dict[keyresult] = [formula_ips]
-
-
-# list of estimations for every estimator from config
-    # bucket : df 
-    # config : {} of results
-#   ..{} -> df 
-
-        # for policy in config['policies']:
-        #     print(policy)
-        # I need r from df as the, and to append the list value of the policy to the key of the policy key
-    return  return_dict # only work in this function!!!!!
-
-# def estimate_bucket_only_ips(bucket: pd.DataFrame, config: dict) -> dict: 
-#     df = pd.DataFrame({'p': [0.5], 'r': [1], 'policy_1': [0.5]})
-#     config = {
-#             'policies':{'policy_1':['ips']}
-#     }
-#     return {'policy_1_ips': [1]}
-
-
-
-# TODO: write what fields are needed for function accurately
-# 1. write specification for all requirements for inputs and outputs ie what is contract
-# 2. tests for the test_estimate unittests using manual formula for validation
-# 3. schema for what estimate package should return (ie definition for names of columns originated from config)
-
-## next PR -> 1. function estimate_bucket (input_data, estimation_config) returns output
-## tests
-## larger goal: end to end with tests
-
+        pass
--- from_mwt_ds/DataScience/tests/test_estimate.py
+++ from_mwt_ds/DataScience/tests/test_estimate.py
@@ -1,46 +1,27 @@
 import unittest, pandas as pd
 from estimate import estimate_bucket
-# df needs to have probability (p), reward (r) and probability of policy to be estimated for counterfactual
 
-# ips = \sum(r * p_est / p) / \sum(1)
-# snips = \sum(r * p_est / p) / \sum(p_est / p)
 
 class TestEstimate(unittest.TestCase):
-    # def test_estimate_bucket(self):
-        # df = pd.DataFrame({'p': [0.5], 'r': [1], 'policy_1': [0.5]})
-        # config = {
-        #     'policies':{'policy_1':['ips','snips']}
-        # }
-
-        # self.assertEqual(
-        #     {'policy_1_ips': [1], 'policy_1_snips': [1]}, estimate_bucket(df, config))
-
     def  test_estimate_bucket_empty(self):
-        df = {}
+        df = pd.DataFrame
         config = {}
         self.assertEqual({}, estimate_bucket({},{}))
 
-    def test_estimate_bucket_only_ips(self): # need to use estimate_bucket
+    def test_estimate_bucket_with_ips(self):
         df = pd.DataFrame({'p': [0.5], 'r': [1], 'policy_1': [0.5]})
-        config = {
+        config = {  
             'policies':{'policy_1':['ips']}
         }
-            # I need r from df as the, and to append the list value of the policy to the key of the policy key
         self.assertEqual(
-            {'policy_1_ips': [1]}, estimate_bucket(df, config)) # work in estimate.py to get this to pass
-            # expects:  {'policy_1_ips': [1]}
-  
-    #     self.assertEqual(
-    #         {'policy_1_ips': [1]}, estimate_bucket(df, config))
-
-    # def test_estimate_bucket_more_sophisticated(self):
-    #     df = pd.DataFrame({'p': [0.5, 0.8], 'r': [1, 0.4], 'policy_1': [0.5, 0]})
-    #     config = {
-    #         'policies':{'policy_1':['ips','snips']}
-    #     }
-
-    #     self.assertEqual(
-    #         {'policy_1_ips': [1], 'policy_1_snips': [1]}, estimate_bucket(df, config))
-
-
+            {'policy_1_ips': [1]}, estimate_bucket(df, config))
     
+    def test_estimate_bucket_ips_and_snips(self):
+        df = pd.DataFrame({'p': [0.5], 'r': [1], 'policy_1': [0.5]})
+        config = {  
+            'policies':{'policy_1':['ips', 'snips']}
+        }
+        self.assertEqual(
+            {'policy_1_ips': [1], 'policy_1_snips': [1]}, estimate_bucket(df, config))
+         
+
