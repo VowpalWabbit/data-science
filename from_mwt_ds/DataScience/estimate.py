3310043e83b646a646a850baca4fb8c7740c99b1
kammitama5 <krystal.maughan@gmail.com>
2021-07-03 19:07:33 -0400

    Updated estimate file imports not working

--- from_mwt_ds/DataScience/estimate.py
+++ from_mwt_ds/DataScience/estimate.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import estimators
-from bandits import ips
+from estimate import estimators
+# these imports ie bandits and snips aren't working for me
+# from estimators import bandits
+# from bandits import snips
 
 def estimate_bucket(bucket: pd.DataFrame, config: dict) -> dict: 
+    # init estimaors
+    ipssnips = snips.Estimator()
     if len(bucket) == 0 or config == {}:
         return {}
     else:
