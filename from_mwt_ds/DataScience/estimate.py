06dfa4dd85f4a3a21f454f660d77dd5c5d441f6b
kammitama5 <krystal.maughan@gmail.com>
2021-07-08 14:56:38 -0400

    Remove process input and add for loop in estimate function

--- from_mwt_ds/DataScience/estimate.py
+++ from_mwt_ds/DataScience/estimate.py
@@ -1,11 +1,6 @@
 import pandas as pd
 from estimators.bandits import ips, snips, mle, cressieread
 
-
-def process_input(input_file, chunksize) -> pd.DataFrame:
-    for chunk in pd.read_csv(input_file, chunksize=chunksize):
-        return chunk
-
 def estimate_bucket(bucket: pd.DataFrame, config: dict) -> dict:
     if len(bucket) == 0 or config == {}:
         return {}
@@ -38,9 +33,8 @@ def estimate_bucket(bucket: pd.DataFrame, config: dict) -> dict:
 def estimate(input_files, config):
     number_of_events = config["aggregation"]['num_of_events']
     for file in input_files:
-        chunksize = number_of_events
-        tmp_df = process_input(file, chunksize)
-        estimate_bucket(tmp_df, config)
+        for chunk in pd.read_csv(file, chunksize=number_of_events):
+            estimate_bucket(chunk, config)
     return 
 
 input_files = ["test_data/cb/01.csv"]
