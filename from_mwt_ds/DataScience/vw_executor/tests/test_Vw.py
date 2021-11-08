323b5eb94591ec864820f3cc352c97a3604d2b84
Alexey Taymanov <ataymano@gmail.com>
2021-11-08 15:11:27 -0500

    more tests

--- from_mwt_ds/DataScience/vw_executor/tests/test_Vw.py
+++ from_mwt_ds/DataScience/vw_executor/tests/test_Vw.py
@@ -1,5 +1,6 @@
 import unittest
 from vw_executor.vw import *
+from vw_executor.vw_opts import Grid
 
 class TestVw(unittest.TestCase):
     input1 = 'vw_executor/tests/data/cb_0.json'
@@ -7,6 +8,7 @@ class TestVw(unittest.TestCase):
 
     def test_data_shape(self):
         vw = Vw('.vw_cache', handlers = [])
+        
         result = vw.train(self.input1, '--cb_explore_adf --dsjson')
         self.assertEqual(isinstance(result, Job), True)
         self.assertEqual(len(result), 1)
@@ -22,4 +24,70 @@ class TestVw(unittest.TestCase):
         self.assertEqual(len(result), 2)
         self.assertEqual(len(result[0]), 1)
         self.assertEqual(len(result[1]), 1)
+
+        result = vw.train(self.input1, [
+            '--cb_explore_adf --dsjson --epsilon 0.1',
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.2}])
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 1)
+        self.assertEqual(len(result[1]), 1)
+
+        result = vw.train(self.input1, [
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.1},
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.2}])
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 1)
+        self.assertEqual(len(result[1]), 1)
+
+        result = vw.train(self.input1, Grid({
+            '#base': ['--cb_explore_adf --dsjson'],
+            '--epsilon': [0.1, 0.2]
+        }))
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 1)
+        self.assertEqual(len(result[1]), 1)
+
+        result = vw.train([self.input1, self.input2], '--cb_explore_adf --dsjson')
+        self.assertEqual(isinstance(result, Job), True)
+        self.assertEqual(len(result), 2)
+
+        result = vw.train([self.input1, self.input2], {'#problem': '--cb_explore_adf', '#format':  '--dsjson'})
+        self.assertEqual(isinstance(result, Job), True)
+        self.assertEqual(len(result), 2)
+
+        result = vw.train([self.input1, self.input2], [
+            '--cb_explore_adf --dsjson --epsilon 0.1',
+            '--cb_explore_adf --epsilon 0.2'])
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(len(result[1]), 2)
+
+        result = vw.train([self.input1, self.input2], [
+            '--cb_explore_adf --dsjson --epsilon 0.1',
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.2}])
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(len(result[1]), 2)
+
+        result = vw.train([self.input1, self.input2], [
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.1},
+            {'#base': '--cb_explore_adf --dsjson', '--epsilon':  0.2}])
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(len(result[1]), 2)
+
+        result = vw.train([self.input1, self.input2], Grid({
+            '#base': ['--cb_explore_adf --dsjson'],
+            '--epsilon': [0.1, 0.2]
+        }))
+        self.assertEqual(isinstance(result, list), True)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(len(result[1]), 2)
 
