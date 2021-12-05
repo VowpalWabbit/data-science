fc72b074f14d907d067f1235254bddf8e8f20e12
Alexey Taymanov <ataymano@gmail.com>
2021-12-04 20:12:47 -0500

    predictions parsing tests

--- from_mwt_ds/DataScience/vw_executor/tests/test_Artifacts.py
+++ from_mwt_ds/DataScience/vw_executor/tests/test_Artifacts.py
@@ -6,3 +6,16 @@ class TestOutput(unittest.TestCase):
         output = Output('vw_executor/tests/data/artifacts/stdout_cb.txt')
         self.assertEqual(output.loss, -0.88)
         self.assertEqual(len(output.loss_table), 10)
+
+class TestPredictions(unittest.TestCase):
+    def test_predictions_scalar(self):
+        predictions = Predictions('vw_executor/tests/data/artifacts/pred_scalar.txt')
+        self.assertEqual(len(predictions.scalar), 21)   
+
+    def test_predictions_cb(self):
+        predictions = Predictions('vw_executor/tests/data/artifacts/pred_cb.txt')
+        self.assertEqual(len(predictions.cb), 10)  
+
+    def test_predictions_ccb(self):
+        predictions = Predictions('vw_executor/tests/data/artifacts/pred_ccb.txt')
+        self.assertEqual(len(predictions.ccb), 22)        
