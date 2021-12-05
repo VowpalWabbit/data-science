d467f481c93eb3cdeee5aca7c421d14ff153fbf6
Alexey Taymanov <ataymano@gmail.com>
2021-12-04 19:46:56 -0500

    stdout test update

--- from_mwt_ds/DataScience/vw_executor/tests/test_Artifacts.py
+++ from_mwt_ds/DataScience/vw_executor/tests/test_Artifacts.py
@@ -5,3 +5,4 @@ class TestOutput(unittest.TestCase):
     def test_output_cb(self):
         output = Output('vw_executor/tests/data/artifacts/stdout_cb.txt')
         self.assertEqual(output.loss, -0.88)
+        self.assertEqual(len(output.loss_table), 10)
