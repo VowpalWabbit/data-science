0d102bd26b16ebf648514915c138f1c5d86e0a0b
Alexey Taymanov <ataymano@gmail.com>
2021-12-10 21:54:37 -0500

    version bump

--- from_mwt_ds/DataScience/setup.py
+++ from_mwt_ds/DataScience/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 MAJOR               = 0
 MINOR               = 1
-MICRO               = 5
+MICRO               = 6
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
