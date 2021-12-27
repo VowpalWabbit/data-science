b220c1fa1a58b51c651e884f165f54db3bf36a8d
Alexey Taymanov <ataymano@gmail.com>
2021-12-27 09:59:08 -0500

    types hints for pool

--- from_mwt_ds/DataScience/vw_executor/pool.py
+++ from_mwt_ds/DataScience/vw_executor/pool.py
@@ -1,6 +1,7 @@
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 
+from typing import Callable, List, Any
 
 def _execute(task_input):
     return task_input[0](*task_input[1])
@@ -10,7 +11,7 @@ class SeqPool:
     def __init__(self):
         self.procs = 1
         
-    def map(self, task, inputs):
+    def map(self, task: Callable, inputs: List[Any]) -> Any:
         result = []
         for i in inputs:
             result.append(task(*i))
@@ -18,10 +19,10 @@ class SeqPool:
 
 
 class MultiThreadPool:
-    def __init__(self, procs=multiprocessing.cpu_count()):
+    def __init__(self, procs: int = multiprocessing.cpu_count()):
         self.procs = procs
 
-    def map(self, task, inputs):
+    def map(self, task: Callable, inputs: List[Any]) -> Any:
         args = [(task, i) for i in inputs]
         with ThreadPool(processes=self.procs) as p:
             return p.map(_execute, args)
