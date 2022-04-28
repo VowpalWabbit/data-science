7788148e5033ffda88f177db9ee21802f58a00ef
Alexey Taymanov <ataymano@gmail.com>
2022-04-27 22:10:53 -0400

    test for nested tags

--- from_mwt_ds/DataScience/vw_executor/tests/test_loggers.py
+++ from_mwt_ds/DataScience/vw_executor/tests/test_loggers.py
@@ -1,4 +1,4 @@
-from vw_executor.loggers import MultiFileLogger
+from vw_executor.loggers import MultiFileLogger, FileLogger
 from pathlib import Path
 import shutil
 
@@ -49,3 +49,38 @@ class TestMultiFileLogger(unittest.TestCase):
             content = f.readlines()
             self.assertEqual(len(content), 1)
             self.assertTrue(content[0].strip().endswith('level2_b'))  
+
+
+class TestFileLogger(unittest.TestCase):
+    def setUp(self):
+        self.log_path = 'test_logs/single_log.txt'
+        if Path(self.log_path).exists():
+            Path(self.log_path).unlink()
+
+    def test_nesting_is_supported(self):
+        root = FileLogger(self.log_path, 'DEBUG')
+        root.debug('root')
+
+        level1_a = root['a']
+        level1_a.debug('level1_a')
+
+        level2_a = level1_a['a']
+        level2_a.debug('level2_a')   
+
+        level2_b = level1_a['b']
+        level2_b.debug('level2_b')
+
+        level1_b = root['b']
+        level1_b.debug('level1_b')      
+
+        def trim_time(line):
+            return line[line.find(']')+1:]
+
+        with open(self.log_path) as f:
+            content = f.readlines()
+            self.assertEqual(len(content), 5)
+            self.assertTrue(trim_time(content[0]), ' root') 
+            self.assertTrue(trim_time(content[1]), '[a] level1_a')  
+            self.assertTrue(trim_time(content[2]), '[a][a] level2_a') 
+            self.assertTrue(trim_time(content[3]), '[a][b] level2_b')  
+            self.assertTrue(trim_time(content[4]), '[b] level1_b') 
